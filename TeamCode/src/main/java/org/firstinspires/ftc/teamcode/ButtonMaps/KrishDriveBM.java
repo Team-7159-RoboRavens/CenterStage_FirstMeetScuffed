72d815dc449e79ace089ec7c22994f9753948ab9
karsteny <42128680+karsteny@users.noreply.github.com>
2023-10-20 15:40:00 -0700

    Create KrishDriveBM.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/KrishDriveBM.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.ButtonMaps;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.ComplexRobots.CenterStageRobot;
+
+public class KrishDriveBM extends AbstractButtonMap{
+    @Override
+    public void loop(CenterStageRobot robot, OpMode opMode) {
+        
+    }
+}
