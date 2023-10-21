09bea016fb3f27f41dcdc7f1f8f09438e3bfc4dc
karsteny <42128680+karsteny@users.noreply.github.com>
2023-10-20 20:33:36 -0700

    Finish Krish Buttonmap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/KrishDriveBM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/KrishDriveBM.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.ButtonMaps;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.ComplexRobots.CenterStageRobot;
 
@@ -11,15 +13,43 @@ public class KrishDriveBM extends AbstractButtonMap{
     private final double slowStrafePower = 0.2;
 
     private boolean buttonPressed = false;
-    private boolean motorBrake = false;
+    private boolean motorBrake = true;
+    private ElapsedTime et = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
 
     private double currentStrafeMotorPower;
     private MotorPowers mp;
 
+
     @Override
     public void loop(CenterStageRobot robot, OpMode opMode) {
+        //Set mp to zero at the start, if no buttons are pressed it will never change
+        mp = new MotorPowers(0);
 
-        //Speed Settings
+        //Motor Brake (toggle) w/ cooldown to avoid rapidly flipping
+        if(opMode.gamepad1.b && et.time() > 500){
+            et.reset();
+            if(motorBrake){
+                robot.leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                robot.leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                robot.rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                robot.rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                motorBrake = false;
+            }else{
+                robot.leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                robot.leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                robot.rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                robot.rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                motorBrake = true;
+            }
+        }
+        //Displaying current motor brake status
+        if(motorBrake){
+            opMode.telemetry.addData("Drive Motor Mode", "Brake");
+        }else{
+            opMode.telemetry.addData("Drive Motor Mode", "Coast");
+        }
+
+        //Speed Settings (hold)
         if(opMode.gamepad1.left_bumper){
             currentStrafeMotorPower = slowStrafePower;
         }else if(opMode.gamepad1.right_bumper){
@@ -32,14 +62,13 @@ public class KrishDriveBM extends AbstractButtonMap{
         MotorPowers dpadMotorPowers = DPadControl.dpadStrafe(opMode.gamepad1, currentStrafeMotorPower);
         MotorPowers triggerMotorPowers = new MotorPowers(0);
         if(opMode.gamepad1.right_trigger > 0.1){
-            //Forward
+            //Right
             triggerMotorPowers = new MotorPowers(0, opMode.gamepad1.right_trigger*currentStrafeMotorPower, 0, opMode.gamepad1.right_trigger*currentStrafeMotorPower);
         }else if(opMode.gamepad1.left_trigger > 0.1){
-            //Backward
+            //Left
             triggerMotorPowers = new MotorPowers(opMode.gamepad1.left_trigger*currentStrafeMotorPower, 0, opMode.gamepad1.left_trigger*currentStrafeMotorPower, 0);
         }
         if(dpadMotorPowers.isNotZero() || triggerMotorPowers.isNotZero()){
-//            buttonPressed = true;
             mp = dpadMotorPowers;
             mp.combineWith(triggerMotorPowers);
         }
@@ -47,13 +76,10 @@ public class KrishDriveBM extends AbstractButtonMap{
         //Field Oriented Drive (joysticks), 1st priority
         MotorPowers fodMotorPowers = FieldOrientedDrive.fieldOrientedDrive(opMode.gamepad1, robot.imu, currentStrafeMotorPower);
         if(fodMotorPowers.isNotZero()){
-//            buttonPressed = true;
             mp = fodMotorPowers;
         }
 
-//        if(!buttonPressed){
-//            mp = new MotorPowers(0);
-//        }
+        //Actually apply the power
         robot.setMotorPowers(mp);
     }
 }
