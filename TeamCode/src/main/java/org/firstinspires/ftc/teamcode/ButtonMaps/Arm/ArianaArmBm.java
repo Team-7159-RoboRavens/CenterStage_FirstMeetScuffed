6e0b9a96d7d5a283cc04e6cccf1f79064eed905f
oversizedFart <84299941+hiPeeps124816@users.noreply.github.com>
2023-11-06 17:46:50 -0800

    Create ArianaArmBm.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Arm/ArianaArmBm.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.ButtonMaps.Arm;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.ButtonMaps.AbstractButtonMap;
+import org.firstinspires.ftc.teamcode.ComplexRobots.CenterStageRobot;
+
+public class ArianaArmBm extends AbstractButtonMap {
+    @Override
+    public void loop(CenterStageRobot robot, OpMode opMode) {
+        
+    }
+}
